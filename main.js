e891957df60e078080400975d17d6120074879c6
Alex Cartura <demonrose66@gmail.com>
2024-09-11 23:31:31 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+let num1 = prompt("scegli il primo numero")
+
+let num2 = prompt("scegli il secondo numero")
+
+let somma = num1 + num2;
+
+let sottrazione = num1 - num2;
+
+let moltiplicazione = num1 * num2;
+
+let divisione = num1 / num2;
+
+let potenza = num1 ** num2;
+
+alert("Con i numeri da te scelti, i risultati delle varie operazioni sono: somma " + somma + ", sottrazione: " + sottrazione + ", moltiplicazione: " + moltiplicazione + ", divisione: " + divisione + ", elevazione a potenza: " + potenza);
+
